94e49fe6b4e3e9be83bb812aafef59a7f120e475
Ville <ville777@protonmail.com>
2023-01-17 23:57:25 +0200

    first

---
+++ exercise1a.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+        printf("C is a\n");
+        printf("\t- general-purpose,\n");
+        printf("\t- procedural computer programming language\n");
+        printf("supporting:\n");
+        printf("\t- structured programming\n");
+        printf("\t- lexical variable scope and\n");
+        printf("\t- recursion,\n");
+        printf("while a static type system prevents unintended operations.\n");
+        printf("By design, C provides\n");
+        printf("\t- constructs that map efficiently to typical machine instructions and\n");
+        printf("\t- has found lasting use in applications previously coded in assembly language.\n");
+}
