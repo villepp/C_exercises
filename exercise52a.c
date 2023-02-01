6d119fd015e79375e42f1af2262112ea61adc434
Ville <ville777@protonmail.com>
2023-02-01 20:48:20 +0200

    5.2 A

---
+++ exercise52a.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#define MAX 70
+#define MIN 20
+
+int ask_integer_number(int min, int max);
+
+int main(void)
+{
+    printf("Give number between 20 and 70: ");
+    int result = ask_integer_number(MIN,MAX);
+    printf("\nUser given number was %d",result);
+}
+
+int ask_integer_number(int min, int max) {
+    int user_number;
+    while (1)
+    {
+        scanf("%d", &user_number);
+        if(user_number>=min && user_number<=max) {
+            return user_number;
+            break;
+        } else {
+            printf("Number was not in range (%d-%d): ",min,max);
+        }
+    }
+    
+}
