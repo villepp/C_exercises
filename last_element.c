6ed37e0040ef635a7c147e0a5f55f745e91b2560
Ville <ville777@protonmail.com>
2023-03-21 21:33:10 +0200

    extra exercises

---
+++ last_element.c
@@ -0,0 +1,17 @@
+#include <stdio.h>    
+
+int main(void)
+{
+    int array[] = {4, 5, 6, 7, 8};
+
+    int size = sizeof(array) / sizeof(array[0]);
+
+    if(size > 0){
+        int lastElement = array[size - 1];
+        printf("Last element of array : %d \n", lastElement);
+    } else {
+        printf("Array is empty \n");
+    }
+
+    return 0;
+}
