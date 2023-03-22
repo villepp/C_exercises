6abdd0607d120a4890db61976a61a9f5fc9d1416
Ville <ville777@protonmail.com>
2023-03-22 21:21:47 +0200

    binary program

---
+++ binary.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void binary(int a)
+{
+    if (a > 1) {
+        binary(a / 2);
+    }
+    printf("%d", a % 2);
+}
+
+
+int main(void){
+    int num = 4;
+    printf("The binary representation of %d is: ", num);
+    binary(num);
+    printf("\n");
+}
