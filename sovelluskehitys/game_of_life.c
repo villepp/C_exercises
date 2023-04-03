e190c422f65e8de23d1d3716d200e37090c4dbb4
Ville <ville777@protonmail.com>
2023-04-03 17:14:34 +0300

    functions, loops, define

--- sovelluskehitys/game_of_life.c
+++ sovelluskehitys/game_of_life.c
@@ -2,130 +2,162 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define MAX 20
+
 struct cell
 {
-    int current;  /* current situation, which is visible on screen */
-    int future;   /* temporary calculation area for next round calculation */
+    int current; /* current situation, which is visible on screen */
+    int future;  /* temporary calculation area for next round calculation */
 };
 
-void create_next_day(struct cell board[10][10]);
+void generate_first_board(struct cell board[MAX][MAX]);
+void create_next_day(struct cell board[MAX][MAX]);
+int count_neighbours(struct cell board[MAX][MAX], int i, int j);
+void show_next_day(struct cell board[MAX][MAX]);
 
 int main(void)
 {
-    int i, j = 0;
+    /* Initialize character used by user to choose whether to continue or not */
+    char continue_choise = 'f';
 
-    struct cell board [10] [10] = {0, 0};
+    /* Initialize board used for the game */
+    struct cell board[MAX][MAX] = {0, 0};
 
-    int random_number = 0;
+    /* Generates first board and shows it to the user */
+    generate_first_board(board);
 
-    srand(time(NULL));
-
-    //board[4][4].current = 1;
-    //board[4][5].current = 1;
-    //board[4][6].current = 1;
-    //board[5][6].current = 1;
-
-    printf("Current board: \n\n");
-
-    for(i=0;i<9;i++)
+    /* Continue showing the next day until user inputs n*/
+    while (continue_choise != 'n')
     {
-        for(j=0;j<9;j++)
-        {
-            random_number = rand() % 5;
-            if (random_number == 0) 
-            {
-                board[i][j].current = 1;
-            } 
-            printf("%d",board[i][j]);
-        }
-        printf("\n");
+        printf("Show the next  day?: y/n ");
+        scanf(" %c", &continue_choise);
+
+        /* Create the next day, count_neighbours function is also called inside this function */
+        create_next_day(board);
+
+        /* Show the next day while also updating cells from future position to current */
+        show_next_day(board);
     }
 
-    //TODO while loop, jatka kunnes boardit samat
-    create_next_day(board);
+    printf("\nProgram stopped\n");
 
     return 0;
 }
 
-void create_next_day(struct cell board[10][10])
+void generate_first_board(struct cell board[MAX][MAX])
 {
     int i, j = 0;
+    int random_number = 0;
 
+    srand(time(NULL));
 
-    /*alive cell*/
-        for(i=1;i<=9;i++)
-        {
-            for(j=1;j<=9;j++)
-            {
+    printf("Current board: \n\n");
 
-                if(board[i][j].current==1)
-                {
-                int sum = 
-                board[i-1][j-1].current + 
-                board[i][j-1].current + 
-                board[i+1][j-1].current + 
-                board[i-1][j].current + 
-                board[i+1][j].current + 
-                board[i-1][j+1].current + 
-                board[i][j+1].current + 
-                board[i+1][j+1].current;
-
-                if(sum == 2 || sum == 3)
-                {
-                    board[i][j].future = 1;
-                }
-                else
-                {
-                    board[i][j].future = 0;
-                }
-            }
-        }
-    }
-
-    /*empty cell*/
-        for(i=1;i<=9;i++)
-        {
-            for(j=1;j<=9;j++)
-            {
-
-              if(board[i][j].current==0)
-              {   
-                int sum = 
-                board[i-1][j-1].current + 
-                board[i][j-1].current + 
-                board[i+1][j-1].current + 
-                board[i-1][j].current + 
-                board[i+1][j].current + 
-                board[i-1][j+1].current + 
-                board[i][j+1].current + 
-                board[i+1][j+1].current;
-
-                if(sum==3)
-                {
-                    board[i][j].future = 1;
-                }
-                else
-                {
-                    board[i][j].future = 0;
-                }
-              }
-            }
-        }
-
-        printf("\n\nNew board : \n\n");
-
-    
-
-    for(i=1;i<=9;i++)
+    for (i = 0; i <= MAX; i++)
     {
-        for(j=1;j<=9;j++)
+        for (j = 0; j <= MAX; j++)
         {
-            printf("%d",board[i][j].future);
+            if (i >= 1 && i < MAX && j >= 1 && j < MAX)
+            {
+                random_number = rand() % 4;
+
+                if (random_number == 1)
+                {
+                    board[i][j].current = 1;
+                }
+
+                printf("%d", board[i][j].current);
+            }
+            /* Print edges of the board */
+            else
+            {
+                printf(".");
+            }
         }
         printf("\n");
     }
-
 }
 
+void create_next_day(struct cell board[MAX][MAX])
+{
+    int i, j = 0;
 
-//tee tänne ehkä erillinen funktio count_neighbours 
+    for (i = 1; i < MAX; i++)
+    {
+        for (j = 1; j < MAX; j++)
+        {
+            /* Alive cell */
+            if (board[i][j].current == 1)
+            {
+                int sum_alive = count_neighbours(board, i, j);
+
+                if (sum_alive == 2 || sum_alive == 3)
+                {
+                    board[i][j].future = 1;
+                }
+                else
+                {
+                    board[i][j].future = 0;
+                }
+            }
+            /* Dead cell */
+            else if (board[i][j].current == 0)
+            {
+                int sum_dead = count_neighbours(board, i, j);
+
+                if (sum_dead == 3)
+                {
+                    board[i][j].future = 1;
+                }
+                else
+                {
+                    board[i][j].future = 0;
+                }
+            }
+            else
+            {
+                printf("\n!!!error!!!\n");
+            }
+        }
+    }
+}
+
+int count_neighbours(struct cell board[MAX][MAX], int i, int j)
+{
+    int sum =
+        board[i - 1][j - 1].current +
+        board[i][j - 1].current +
+        board[i + 1][j - 1].current +
+        board[i - 1][j].current +
+        board[i + 1][j].current +
+        board[i - 1][j + 1].current +
+        board[i][j + 1].current +
+        board[i + 1][j + 1].current;
+
+    return sum;
+}
+
+void show_next_day(struct cell board[MAX][MAX])
+{
+    int i, j = 0;
+
+    printf("\n\nNew board : \n\n");
+
+    for (i = 0; i <= MAX; i++)
+    {
+        for (j = 0; j <= MAX; j++)
+        {
+            if (i >= 1 && i < MAX && j >= 1 && j < MAX)
+            {
+                board[i][j].current = board[i][j].future;
+                printf("%d", board[i][j].current);
+            }
+            /* Print edges of the board */
+            else
+            {
+                printf(".");
+            }
+        }
+        printf("\n");
+    }
+}
